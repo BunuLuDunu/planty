0fc05d03dbc935f63d998500a1a1b9777580dd6a
BunuLuDunu <laurine.sassano@gmail.com>
2024-02-07 11:35:58 +0100

    Changed theme from Blank Slate to Astra

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -1,11 +1,9 @@
 <?php
 
-// Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
-    // Charge le fichier CSS du thème parent
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    // Charge le fichier theme.css pour les personnalisations
-    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css'));
+
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
